322442f14adc127b1d28be10927d3b3e4d78a63c
90felipe09 <felhipeshiraishi@gmail.com>
2020-06-22 11:46:47 -0300

    Setup: Setup aula 6

---
+++ src/aula6/4-more-stack.s
@@ -0,0 +1,27 @@
+@Esse código foi desenvolvido para a aula 6 do curso PCS3732- Laboratório de Processadores - 2020
+@Feito pelos alunos:
+@Felipe Kenzo Shiraishi - 10262700
+@Hector Kobayashi Yassuda - 10333289
+@Vitor Hugo Perles - 9285492
+
+@Para compilar
+@arm-elf-gcc -Wall -g -o ../bin/aula6/4-more-stack 4-more-stack.s
+
+@To run and debug:
+@gdb 4-more-stack
+@na pasta bin/aula6
+@(gdb) target sim
+@(gdb) load
+@(gdb) break main
+@(gdb) run
+@(gdb) continue
+
+    .text
+    .global main
+ 
+main:
+
+end:
+    MOV r7, r0            @ breakpoint before swi
+    SWI 0x123456
+
